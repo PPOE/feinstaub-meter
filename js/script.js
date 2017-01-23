d21facd6ff0be8d75c0d2fd529322c6c2e4e6b04
PeterTheOne <petertheone@gmail.com>
2017-01-23 11:52:38 +0100

    activate share links

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 
   $(function () {
 
+    if (navigator.userAgent.match(/iPhone|iPad|Android/i)) {
+      $('.share-btn.whatsapp,.share-btn.facebook-messenger').css('display', 'inline-block');
+    }
+
     $('button').on('click', function (event) {
       if ($('article').hasClass('show')) {
         $('article').removeClass('show');
